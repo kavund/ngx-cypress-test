aba3fcba24290eaf045fe0df2c156ca5ad3c2364
kavun1 <steel4111@gmail.com>
2024-10-29 11:18:00 +0200

    cypress intro

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,9 +3,9 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   projectId: '8b7x8g',
   e2e: {
-    retries: {openMode: 0, runMode: 1},
+    retries: {openMode: 0, runMode: 2},
     baseUrl: 'http://localhost:4200/',
-    specPattern: 'cypress/e2e/**/*.spec.{js,ts}',
+    specPattern: 'cypress/e2e/**/*.{spec,cy}.{js,ts}',
 
     setupNodeEvents(on, config) {
       // implement node event listeners here
--- cypress/e2e/steps/steps.cy.js
+++ cypress/e2e/steps/steps.cy.js
@@ -1,17 +1,18 @@
 describe('Steps page', ()=>{
-    it('should show valid title on each step', ()=>{
+    it('should show valid title on each step', ()=>{ // замість it можна також писати test
+        cy.visit ('/pages/layout/stepper')
         const containerSelector = 'nb-card-body nb-stepper.horizontal[orientation="horizontal"]'
 
         cy.get(`${containerSelector} h3`).should('have.text', "Step content #1")
-        cy.get(`${containerSelector} [type="submit"]`).click()
+        cy.get(`${containerSelector}`).contains('next').click()
 
         cy.get(`${containerSelector} h3`).should('have.text', "Step content #2")
-        cy.get(`${containerSelector} [type="submit"]`).click()
+        cy.get(`${containerSelector}`).contains('next').click()
 
         cy.get(`${containerSelector} h3`).should('have.text', "Step content #3")
-        cy.get(`${containerSelector} [type="submit"]`).click()
+        cy.get(`${containerSelector}`).contains('next').click()
 
         cy.get(`${containerSelector} h3`).should('have.text', "Step content #4")
-        cy.get(`${containerSelector} [type="submit"]`).should('be.disabled')
+        cy.get(`${containerSelector}`).contains('next').should('be.disabled')
     });
 });
